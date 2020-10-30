da36f1ccc100fe2bb97f8f38ce9f205e1c2b729c
igniushka <ignasxp@gmail.com>
2020-10-30 19:14:30 +0000

    bounding operator created

--- main.cpp
+++ main.cpp
@@ -9,13 +9,111 @@
 #include <iterator>
 #include <list>
 #include <string>
+#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
+
+struct Node {
+   vector<int> scheduled;
+   vector<int> left;
+};
 
 struct FSPspace {
   int machines;
   int jobs;
   int** operations;
 };
+
+int bound(FSPspace flowshop, Node node){
+   vector<int> bounds;
+   vector<int> outTimes;
+   //initialize out times
+   for (int s = 0; s < node.scheduled.size(); s++)  outTimes.push_back(0);
+
+   for(int m = 0; m < flowshop.machines; m++){        //for all machines
+      
+      for (int s = 0; s < node.scheduled.size(); s++){ //for calculate scheduled job times
+         int j = node.scheduled[s];
+         int procedureTime = flowshop.operations[m][j];
+         if (s == 0){
+            outTimes[s] += procedureTime;  //first element does not ever wait
+         } else {
+           int wait = outTimes[s-1] - outTimes[s]; 
+            if (wait > 0) { //positive wait time means procedure has to wait for machine to be free
+               outTimes[s] += wait + procedureTime;
+            } else {
+               outTimes[s] += procedureTime;
+            }
+         }
+      } 
+
+      int bound = outTimes.back();
+      vector<int> estimates;
+      for (int l = 0; l < node.left.size(); l++){
+         int j = node.left[l];
+         bound += flowshop.operations[m][j];
+         // only compute the future operations of unsheduled if the machine is not last
+         if (m != flowshop.machines -1){
+            int unscheduledOperations = 0;
+            for (int op = m+1; op < flowshop.machines; op++){
+               unscheduledOperations += flowshop.operations[op][j];
+            }
+            estimates.push_back(unscheduledOperations);
+         }
+      }
+
+      if(!estimates.empty()){
+         bound += *min_element(estimates.begin(), estimates.end());
+      }
+      bounds.push_back(bound);
+   }
+   return *max_element(bounds.begin(), bounds.end());
+}
+
+void solve(FSPspace flowshop){
+   //initialize problem
+   int ub = -1;
+   vector<int> solution;
+   vector<Node> problems;
+   Node root;
+   for (int i = 0; i < flowshop.jobs; i++) root.left.push_back(i);
+
+   problems.push_back(root);
+   //solve problem recursively
+   while (problems.size() > 0){
+      Node node = problems.back();
+      problems.pop_back();
+      
+      // if there are more than 1 jobs left unscheduled and uper bound is known
+      if (node.left.size() > 1){
+         if (ub != -1){
+            int lb = bound(flowshop, node); //get lb
+            if (lb < ub){
+               for (int i =0; i < node.left.size(); i++){
+                  Node child;
+                  child.scheduled.assign(node.scheduled.begin(), node.scheduled.end());
+                  child.scheduled.push_back(node.left[i]); // schedule i'th element
+                  child.left.assign(node.left.begin(), node.left.end()); // assign unscheduled jobs from parent
+                  child.left.erase(child.left.begin()+i); // erase i'th element
+               }
+            }
+         }
+
+      } else { // if one node is left
+          int candidateBound = bound(flowshop, node);
+          if (candidateBound < ub || ub == -1){
+            ub = candidateBound;
+            solution.assign(node.scheduled.begin(), node.scheduled.end());
+            solution.push_back(node.left.back());
+            }
+         } 
+      }
+      cout << "Solution time: " << ub <<"\n";
+      for(int i =0; i < solution.size(); i++) cout << solution[i] << " ";
+         cout << "\n";
+   }
+
 
 int invalidFormatError(string message){
    cout<<"input file has invalid format: "<<message<<"\n";
@@ -44,7 +142,7 @@ int* parseLine(string line, int expected){
       return convertListStingToInt(chunks);
 }
 
-void parseFile(string fileName){
+FSPspace parseFile(string fileName){
    int* chunks;
    ifstream file;
    file.open(fileName);
@@ -73,6 +171,7 @@ void parseFile(string fileName){
             }
                cout<<"\n";
             }
+            return flowshop;
 
       } else {
       printf("could not open file\n");
@@ -84,11 +183,11 @@ void parseFile(string fileName){
    }
 }
 
-
 int main(int argc, char* argv[]) {   
    if (argc < 2){
       printf("input file not provided\n");
       exit(EXIT_FAILURE);
    }
-         parseFile(argv[1]);
+      FSPspace flowshop = parseFile(argv[1]);
+
 }
