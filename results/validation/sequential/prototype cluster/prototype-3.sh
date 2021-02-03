c6db56cfe54a91ccb236d2ef929bf10d5c7397b5
igniushka <ignasxp@gmail.com>
2021-02-03 13:50:51 +0000

    results added

---
+++ results/validation/sequential/prototype cluster/prototype-3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./prototype ../../../FSPinstances/ta30 >> prot/3/ta30
+./prototype ../../../FSPinstances/ta29 >> prot/3/ta29
+./prototype ../../../FSPinstances/ta28 >> prot/3/ta28
+./prototype ../../../FSPinstances/ta27 >> prot/3/ta27
+./prototype ../../../FSPinstances/ta26 >> prot/3/ta26
+./prototype ../../../FSPinstances/ta25 >> prot/3/ta25
+./prototype ../../../FSPinstances/ta24 >> prot/3/ta24
+./prototype ../../../FSPinstances/ta23 >> prot/3/ta23
+./prototype ../../../FSPinstances/ta22 >> prot/3/ta22
+./prototype ../../../FSPinstances/ta21 >> prot/3/ta21
