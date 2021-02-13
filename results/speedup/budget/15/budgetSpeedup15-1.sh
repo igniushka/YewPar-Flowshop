09f828e691747593fbda3d53a24b8717a4428208
igniushka <ignasxp@gmail.com>
2021-02-13 18:51:11 +0000

    results added

---
+++ results/speedup/budget/15/budgetSpeedup15-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./flowshop -f ../../../FSPinstances/ta30 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta30-b50000-16
+./flowshop -f ../../../FSPinstances/ta29 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta29-b50000-16
+./flowshop -f ../../../FSPinstances/ta28 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta28-b50000-16
+./flowshop -f ../../../FSPinstances/ta27 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta27-b50000-16
+./flowshop -f ../../../FSPinstances/ta26 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta26-b50000-16
+./flowshop -f ../../../FSPinstances/ta25 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta25-b50000-16
+./flowshop -f ../../../FSPinstances/ta24 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta24-b50000-16
+./flowshop -f ../../../FSPinstances/ta23 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta23-b50000-16
+./flowshop -f ../../../FSPinstances/ta22 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta22-b50000-16
+./flowshop -f ../../../FSPinstances/ta21 --skel budget -b 50000 --hpx:threads 16 >> ./budget/15/1/ta21-b50000-16
