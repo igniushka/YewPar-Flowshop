7946b825ed44319ecf64b3d63fa5705f14ca6641
igniushka <IgnasXP@gmail.com>
2021-07-10 13:08:39 +0300

    instances aded

---
+++ scripts/input-converter/script.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+./a.out ../instances/Small/VFR10_5_1_Gap.txt >> ../instances/VFR/VRF10_5_1
+./a.out ../instances/Small/VFR10_5_2_Gap.txt >> ../instances/VFR/VRF10_5_2
+./a.out ../instances/Small/VFR10_5_3_Gap.txt >> ../instances/VFR/VRF10_5_3
+./a.out ../instances/Small/VFR10_5_4_Gap.txt >> ../instances/VFR/VRF10_5_4
+./a.out ../instances/Small/VFR10_5_5_Gap.txt >> ../instances/VFR/VRF10_5_5
+./a.out ../instances/Small/VFR10_5_6_Gap.txt >> ../instances/VFR/VRF10_5_6
+./a.out ../instances/Small/VFR10_5_7_Gap.txt >> ../instances/VFR/VRF10_5_7
+./a.out ../instances/Small/VFR10_5_8_Gap.txt >> ../instances/VFR/VRF10_5_8
+./a.out ../instances/Small/VFR10_5_9_Gap.txt >> ../instances/VFR/VRF10_5_9
+./a.out ../instances/Small/VFR10_5_10_Gap.txt >> ../instances/VFR/VRF10_5_10
+
+./a.out ../instances/Small/VFR10_10_1_Gap.txt >> ../instances/VFR/VFR10_10_1
+./a.out ../instances/Small/VFR10_10_2_Gap.txt >> ../instances/VFR/VFR10_10_2
+./a.out ../instances/Small/VFR10_10_3_Gap.txt >> ../instances/VFR/VFR10_10_3
+./a.out ../instances/Small/VFR10_10_4_Gap.txt >> ../instances/VFR/VFR10_10_4
+./a.out ../instances/Small/VFR10_10_5_Gap.txt >> ../instances/VFR/VFR10_10_5
+./a.out ../instances/Small/VFR10_10_6_Gap.txt >> ../instances/VFR/VFR10_10_6
+./a.out ../instances/Small/VFR10_10_7_Gap.txt >> ../instances/VFR/VFR10_10_7
+./a.out ../instances/Small/VFR10_10_8_Gap.txt >> ../instances/VFR/VFR10_10_8
+./a.out ../instances/Small/VFR10_10_9_Gap.txt >> ../instances/VFR/VFR10_10_9
+./a.out ../instances/Small/VFR10_10_10_Gap.txt >> ../instances/VFR/VFR10_10_10
+
+./a.out ../instances/Small/VFR10_15_1_Gap.txt >> ../instances/VFR/VFR10_15_1
+./a.out ../instances/Small/VFR10_15_2_Gap.txt >> ../instances/VFR/VFR10_15_2
+./a.out ../instances/Small/VFR10_15_3_Gap.txt >> ../instances/VFR/VFR10_15_3
+./a.out ../instances/Small/VFR10_15_4_Gap.txt >> ../instances/VFR/VFR10_15_4
+./a.out ../instances/Small/VFR10_15_5_Gap.txt >> ../instances/VFR/VFR10_15_5
+./a.out ../instances/Small/VFR10_15_6_Gap.txt >> ../instances/VFR/VFR10_15_6
+./a.out ../instances/Small/VFR10_15_7_Gap.txt >> ../instances/VFR/VFR10_15_7
+./a.out ../instances/Small/VFR10_15_8_Gap.txt >> ../instances/VFR/VFR10_15_8
+./a.out ../instances/Small/VFR10_15_9_Gap.txt >> ../instances/VFR/VFR10_15_9
+./a.out ../instances/Small/VFR10_15_10_Gap.txt >> ../instances/VFR/VFR10_15_10
+
+./a.out ../instances/Small/VFR10_20_1_Gap.txt >> ../instances/VFR/VFR10_20_1
+./a.out ../instances/Small/VFR10_20_2_Gap.txt >> ../instances/VFR/VFR10_20_2
+./a.out ../instances/Small/VFR10_20_3_Gap.txt >> ../instances/VFR/VFR10_20_3
+./a.out ../instances/Small/VFR10_20_4_Gap.txt >> ../instances/VFR/VFR10_20_4
+./a.out ../instances/Small/VFR10_20_5_Gap.txt >> ../instances/VFR/VFR10_20_5
+./a.out ../instances/Small/VFR10_20_6_Gap.txt >> ../instances/VFR/VFR10_20_6
+./a.out ../instances/Small/VFR10_20_7_Gap.txt >> ../instances/VFR/VFR10_20_7
+./a.out ../instances/Small/VFR10_20_8_Gap.txt >> ../instances/VFR/VFR10_20_8
+./a.out ../instances/Small/VFR10_20_9_Gap.txt >> ../instances/VFR/VFR10_20_9
+./a.out ../instances/Small/VFR10_20_10_Gap.txt >> ../instances/VFR/VFR10_20_10
+
+./a.out ../instances/Small/VFR20_5_1_Gap.txt >> ../instances/VFR/VFR20_5_1
+./a.out ../instances/Small/VFR20_5_2_Gap.txt >> ../instances/VFR/VFR20_5_2
+./a.out ../instances/Small/VFR20_5_3_Gap.txt >> ../instances/VFR/VFR20_5_3
+./a.out ../instances/Small/VFR20_5_4_Gap.txt >> ../instances/VFR/VFR20_5_4
+./a.out ../instances/Small/VFR20_5_5_Gap.txt >> ../instances/VFR/VFR20_5_5
+./a.out ../instances/Small/VFR20_5_6_Gap.txt >> ../instances/VFR/VFR20_5_6
+./a.out ../instances/Small/VFR20_5_7_Gap.txt >> ../instances/VFR/VFR20_5_7
+./a.out ../instances/Small/VFR20_5_8_Gap.txt >> ../instances/VFR/VFR20_5_8
+./a.out ../instances/Small/VFR20_5_9_Gap.txt >> ../instances/VFR/VFR20_5_9
+./a.out ../instances/Small/VFR20_5_10_Gap.txt >> ../instances/VFR/VFR20_5_10
+
+./a.out ../instances/Small/VFR20_10_1_Gap.txt >> ../instances/VFR/VFR20_10_1
+./a.out ../instances/Small/VFR20_10_2_Gap.txt >> ../instances/VFR/VFR20_10_2
+./a.out ../instances/Small/VFR20_10_3_Gap.txt >> ../instances/VFR/VFR20_10_3
+./a.out ../instances/Small/VFR20_10_4_Gap.txt >> ../instances/VFR/VFR20_10_4
+./a.out ../instances/Small/VFR20_10_5_Gap.txt >> ../instances/VFR/VFR20_10_5
+./a.out ../instances/Small/VFR20_10_6_Gap.txt >> ../instances/VFR/VFR20_10_6
+./a.out ../instances/Small/VFR20_10_7_Gap.txt >> ../instances/VFR/VFR20_10_7
+./a.out ../instances/Small/VFR20_10_8_Gap.txt >> ../instances/VFR/VFR20_10_8
+./a.out ../instances/Small/VFR20_10_9_Gap.txt >> ../instances/VFR/VFR20_10_9
+./a.out ../instances/Small/VFR20_10_10_Gap.txt >> ../instances/VFR/VFR20_10_10
+
+./a.out ../instances/Small/VFR20_15_1_Gap.txt >> ../instances/VFR/VFR20_15_1
+./a.out ../instances/Small/VFR20_15_2_Gap.txt >> ../instances/VFR/VFR20_15_2
+./a.out ../instances/Small/VFR20_15_3_Gap.txt >> ../instances/VFR/VFR20_15_3
+./a.out ../instances/Small/VFR20_15_4_Gap.txt >> ../instances/VFR/VFR20_15_4
+./a.out ../instances/Small/VFR20_15_5_Gap.txt >> ../instances/VFR/VFR20_15_5
+./a.out ../instances/Small/VFR20_15_6_Gap.txt >> ../instances/VFR/VFR20_15_6
+./a.out ../instances/Small/VFR20_15_7_Gap.txt >> ../instances/VFR/VFR20_15_7
+./a.out ../instances/Small/VFR20_15_8_Gap.txt >> ../instances/VFR/VFR20_15_8
+./a.out ../instances/Small/VFR20_15_9_Gap.txt >> ../instances/VFR/VFR20_15_9
+./a.out ../instances/Small/VFR20_15_10_Gap.txt >> ../instances/VFR/VFR20_15_10
+
+
+
+./a.out ../instances/Small/VFR20_20_1_Gap.txt >> ../instances/VFR/VFR20_20_1
+./a.out ../instances/Small/VFR20_20_2_Gap.txt >> ../instances/VFR/VFR20_20_2
+./a.out ../instances/Small/VFR20_20_3_Gap.txt >> ../instances/VFR/VFR20_20_3
+./a.out ../instances/Small/VFR20_20_4_Gap.txt >> ../instances/VFR/VFR20_20_4
+./a.out ../instances/Small/VFR20_20_5_Gap.txt >> ../instances/VFR/VFR20_20_5
+./a.out ../instances/Small/VFR20_20_6_Gap.txt >> ../instances/VFR/VFR20_20_6
+./a.out ../instances/Small/VFR20_20_7_Gap.txt >> ../instances/VFR/VFR20_20_7
+./a.out ../instances/Small/VFR20_20_8_Gap.txt >> ../instances/VFR/VFR20_20_8
+./a.out ../instances/Small/VFR20_20_9_Gap.txt >> ../instances/VFR/VFR20_20_9
+./a.out ../instances/Small/VFR20_20_10_Gap.txt >> ../instances/VFR/VFR20_20_10
+
+
+
+
+
